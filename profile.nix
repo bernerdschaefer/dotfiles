7f99b3c7e9878d7445171d4ec716a19ebcb5b3b0
Bernerd Schaefer <bj.schaefer@gmail.com>
2014-09-18 17:51:35 +0200

    silversearcher derivation

--- profile.nix
+++ profile.nix
@@ -1,6 +1,6 @@
 let
-     pkgs = import <nixpkgs> {};
-     stdenv = pkgs.stdenv;
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
 in rec {
   # TODO: macvim currently won't build for me
   # macvim = pkgs.macvim;
@@ -19,19 +19,25 @@ in rec {
     name = "silversearcher-${version}";
     version = "0.24.1";
 
-    src = fetchurl {
+    src = pkgs.fetchurl {
       url = "https://github.com/ggreer/the_silver_searcher/archive/0.24.1.tar.gz";
       sha256 = "84e6613d2b774926c5ecce996b9809bfb1c16e4294cbfc0a3dc97a1006cfa2a4";
     };
 
-    buildInputs = [ automake lzma ];
+    nativeBuildInputs = [ pkgs.pkgconfig pkgs.autoconf pkgs.automake ];
+    buildInputs = [ pkgs.lzma pkgs.pcre];
 
-    buildPhase = "./build.sh";
+    preConfigure = ''
+      ${pkgs.automake}/bin/aclocal
+      ${pkgs.autoconf}/bin/autoconf
+      ${pkgs.autoconf}/bin/autoheader
+      ${pkgs.automake}/bin/automake --add-missing
+    '';
 
     meta = with stdenv.lib; {
       description = "The Silver Searcher is a tool for searching code";
       homepage = "http://geoff.greer.fm/ag/";
-      license = licenses.apache2;
+      license = licenses.asl20;
       platforms = platforms.unix;
     };
   };
