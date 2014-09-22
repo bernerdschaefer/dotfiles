be50d64577332acafdba118ead83fc5bac14f31f
Bernerd Schaefer <bj.schaefer@gmail.com>
2014-09-22 12:38:00 +0200

    Add rcm derication

--- profile.nix
+++ profile.nix
@@ -44,4 +44,21 @@ in rec {
       platforms = platforms.unix;
     };
   };
+
+  rcm = stdenv.mkDerivation rec {
+    name = "rcm-${version}";
+    version = "1.2.3";
+
+    src = pkgs.fetchurl {
+      url = "https://thoughtbot.github.io/rcm/dist/rcm-1.2.3.tar.gz";
+      sha256 = "502fd44e567ed0cfd00fb89ccc257dac8d6eb5d003f121299b5294c01665973f";
+    };
+
+    meta = with stdenv.lib; {
+      description = "rc file (dotfile) management";
+      homepage = "http://thoughtbot.github.io/rcm";
+      license = licenses.bsd3;
+      platforms = platforms.unix;
+    };
+  };
 }
