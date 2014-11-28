25e334bf659c9e069e6fb1c7e02502325cd76029
Bernerd Schaefer <b@thoughtbot.com>
2014-11-28 11:53:30 -0800

    Add nixos vagrant config

---
+++ nixos/configuration.nix
@@ -0,0 +1,45 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda";
+
+  # disable journal checking in virtualbox environment
+  boot.initrd.checkJournalingFS = false;
+
+  # enable sshd
+  services.openssh.enable = true;
+
+  # enable virtualbox guest additions
+  services.virtualbox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
+  # passwordless sudo for wheel users
+  security.sudo.wheelNeedsPassword = false;
+
+  # ssh -p2222 -i ~/.ssh/id_vagrant localhost
+  users.extraUsers.bernerd =
+    { createHome = true;
+      home = "/home/bernerd";
+      description = "Bernerd";
+      extraGroups = [ "wheel" ];
+      useDefaultShell = true;
+      openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" ];
+    };
+}
