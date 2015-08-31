805018831947ef1f14d48a862003b20dba9c0d17
Bernerd Schaefer <bj.schaefer@gmail.com>
2015-08-31 12:21:42 -0700

    Add tmate package

--- profile.nix
+++ profile.nix
@@ -16,6 +16,8 @@ in rec {
 
   heroku = pkgs.callPackage ./nix/heroku-toolbelt {};
 
+  tmate = pkgs.callPackage ./nix/tmate {};
+
   parity = pkgs.callPackage ./nix/parity {
     heroku = heroku;
   };
