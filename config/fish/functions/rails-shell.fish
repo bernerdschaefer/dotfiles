1915618b4e03d578fde4f7f7e39feb618dada9d6
Bernerd Schaefer <bj.schaefer@gmail.com>
2015-10-01 09:31:29 -0700

    Fix rails-shell function

--- config/fish/functions/rails-shell.fish
+++ config/fish/functions/rails-shell.fish
@@ -1,4 +1,4 @@
-function rails_shell -d "Start a nix-shell for rails development"
+function rails-shell -d "Start a nix-shell for rails development"
   rm -f qmake-rails-env/bin/qmake
   nix-shell -I ~/.nix-defexpr/channels ~/.rails-profile.nix
 end
