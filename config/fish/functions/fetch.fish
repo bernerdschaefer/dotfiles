c24563c1777f3db7b1f2fdf469983450064d3cca
Bernerd Schaefer <bj.schaefer@gmail.com>
2018-03-26 11:24:42 -0700

    fish: fix filename for fetch function

--- config/fish/functions/fetch.fish
+++ config/fish/functions/fetch.fish
@@ -1,10 +1,12 @@
 function fetch -d "Download an extract a file"
-  switch $1
+  set file $argv[1]
+
+  switch "$file"
     case *.tar.gz
-      curl -L $1 | tar -zxv
+      curl -L "$file" | tar -zxv
     case *.tar.bz2
-      curl -L $1 | tar -jxv
+      curl -L "$file" | tar -jxv
     case *.zip
-      curl -L $1 > tmp.zip; and unzip tmp.zip; and rm tmp.zip
+      curl -L "$file" > tmp.zip; and unzip tmp.zip; and rm tmp.zip
   end
 end
